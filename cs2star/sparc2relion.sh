d5fd19988e576dfcfe4325e75f826dcd5a33b907
kkf <33196427+phoeningo@users.noreply.github.com>
2021-01-07 16:22:00 +0800

    Create sparc2relion.sh

---
+++ cs2star/sparc2relion.sh
@@ -0,0 +1,43 @@
+#Author : Kong Fang
+input=$1
+# change x ,y if needed
+x=3838
+y=3710
+
+cordx_line=` awk '{if ($1=="_rlnCoordinateX") print $2}' ${input} |sed 's/#//' `
+echo cordx_line : ${cordx_line}
+
+cordy_line=` awk '{if ($1=="_rlnCoordinateY") print $2}' ${input} |sed 's/#//' `
+echo cordy_line : ${cordy_line}
+
+  orix=` awk '{if ($1=="_rlnOriginX") print $2}' ${input} |sed 's/#//' `
+  echo orix : ${orix}
+
+  oriy=` awk '{if ($1=="_rlnOriginY") print $2}' ${input} |sed 's/#//' `
+  echo oriy : ${oriy}
+ 
+
+` head -n 50 ${input} | grep mrc | awk '{print NF}' | tail -1 >tmp_r2s `
+tmp_ex=`cat tmp_r2s`
+tmp_ex=`expr $tmp_ex + 1 `
+echo tmp_ex : ${tmp_ex}
+rm tmp_r2s
+
+
+`head -n 50 ${input} |grep -v mrc > tmp_head.star`
+grep mrc $1 >tmp_c.star
+
+`awk '  {$'${cordx_line}'=$'${cordx_line}'*'$x'/'$y';$'${cordy_line}'=$'${cordy_line}'*'$y'/'$x';print $0}' tmp_c.star >tmp1.star`
+
+`awk '  {$'${orix}'=$'${orix}'*'$x'/'$y';$'${oriy}'=$'${oriy}'*'$y'/'$x';print $0} ' tmp1.star>tmp2.star`
+
+#mv tmp2.star $2
+#rm tmp1.star
+#exit
+
+cat tmp_head.star > $2
+cat tmp2.star >>$2
+rm tmp1.star
+rm tmp_c.star
+rm tmp2.star
+rm tmp_head.star
